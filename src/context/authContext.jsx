2d6c8543f50af08e6d390dc690315b3f5971354d
chandra_fajar <chandra@stellardata.ai>
2024-09-12 14:59:33 +0700

    fix build

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -8,25 +8,28 @@ const AuthProvider = ({ children }) => {
     user: null,
     token: "",
   });
-  //default
-  axios.defaults.headers.common["Authorization"] = auth?.token;
 
   useEffect(() => {
+    // Ambil data dari localStorage
     const data = localStorage.getItem("auth");
     if (data) {
       const parseData = JSON.parse(data);
       setAuth({
-        ...auth,
         user: parseData.user,
         token: parseData.token,
       });
     }
   }, []);
 
+  useEffect(() => {
+    // Perbarui header Authorization ketika auth token berubah
+    axios.defaults.headers.common["Authorization"] = auth.token;
+  }, [auth.token]);
+
   return <AuthContext.Provider value={[auth, setAuth]}>{children}</AuthContext.Provider>;
 };
 
-//custom hook
+// Custom hook
 const useAuth = () => useContext(AuthContext);
 
 export { useAuth, AuthProvider };
