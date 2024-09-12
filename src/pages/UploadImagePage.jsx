b0c1bbe5eda6f344bb364f469f0710442026941e
chandra_fajar <chandra@stellardata.ai>
2024-09-12 15:54:03 +0700

    fix bug service

--- src/pages/UploadImagePage.jsx
+++ src/pages/UploadImagePage.jsx
@@ -25,7 +25,7 @@ export default function UploadImagePage() {
 
         if (response.data && response.data.image && response.data.image.id) {
           const imageId = response.data.image.id;
-          setImageUrl(`${process.env.REACT_APP_API_URL}/api/v1/images${imageId}`);
+          setImageUrl(`https://upload-image-be.vercel.app/api/v1/images/${imageId}`);
           toast.success(response.data.message);
         }
       } catch (error) {
